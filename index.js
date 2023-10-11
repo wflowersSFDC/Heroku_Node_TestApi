48f21af27949152b11b6b51cb769de8a26157ffa
Trey Flowers <wflowers@salesforce.com>
2023-10-11 14:55:04 -0400

    rest api

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-/* 
-    Incase you are using mongodb atlas database uncomment below line
-    and replace "mongoAtlasUri" with your mongodb atlas uri.
-*/
-// mongoose.connect( mongoAtlasUri, {useNewUrlParser: true, useUnifiedTopology: true})
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let products = {
+  1: {
+    id: '1',
+    description: 'Widget',
+  },
+  2: {
+    id: '2',
+    description: 'Thing',
+  },
+};
+app.get('/products', (req, res) => {
+  return res.send(Object.values(products));
+}); 
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
