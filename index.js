3ac889ed9caadb05c75df9bafc76bafca4902c0b
Trey Flowers <wflowers@salesforce.com>
2023-10-12 18:25:10 -0400

    retiring products

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/products', (req, res) => {
-  let products = data.getProducts();
-  return res.send(products[Math.floor(Math.random()*products.length)]);
-}); 
+// app.get('/products', (req, res) => {
+//   let products = data.getProducts();
+//   return res.send(products[Math.floor(Math.random()*products.length)]);
+// }); 
 
 app.get('/invoices', (req, res) => {
   return res.send(data.getInvoices());
