1e8e8acbd41bc77af0f34d358a1aaabf2e77f5c9
Trey Flowers <wflowers@salesforce.com>
2023-10-12 18:38:52 -0400

    lining up weather to kwh usage

--- mockdata.js
+++ mockdata.js
@@ -7,81 +7,68 @@ function getWeather(){
 let weather = [
   {
     "Month": "Sep2022",
-    "High": 83,
-    "Low": 63,
-    "Avg": 76
+    "Historical Avg": 76,
+    "Actual Avg": 78
   },
   {
     "Month": "Oct2022",
-    "High": 73,
-    "Low": 50,
-    "Avg": 54
+    "Historical Avg": 54,
+    "Actual Avg": 54
   },
   {
     "Month": "Nov2022",
-    "High": 63,
-    "Low": 40,
-    "Avg": 48
+    "Historical Avg": 48,
+    "Actual Avg": 47
   },
   {
     "Month": "Dec2022",
-    "High": 55,
-    "Low": 34,
-    "Avg": 41
+    "Historical Avg": 41,
+    "Actual Avg": 37
   },
   {
     "Month": "Jan2023",
-    "High": 52,
-    "Low": 32,
-    "Avg": 39
+    "Historical Avg": 39,
+    "Actual Avg": 41
   },
   {
     "Month": "Feb2023",
-    "High": 56,
-    "Low": 34,
-    "Avg": 54
+    "Historical Avg": 54,
+    "Actual Avg": 50
   },
   {
     "Month": "Mar2023",
-    "High": 63,
-    "Low": 40,
-    "Avg": 45
+    "Historical Avg": 45,
+    "Actual Avg": 45
   },
   {
     "Month": "Apr2023",
-    "High": 73,
-    "Low": 49,
-    "Avg": 61
+    "Historical Avg": 61,
+    "Actual Avg": 65
   },
   {
     "Month": "May2023",
-    "High": 80,
-    "Low": 58,
-    "Avg": 73
+    "Historical Avg": 73,
+    "Actual Avg": 70
   },
   {
     "Month": "Jun2023",
-    "High": 87,
-    "Low": 66,
-    "Avg": 69
+    "Historical Avg": 69,
+    "Actual Avg": 72
   },
   {
     "Month": "Jul2023",
-    "High": 91,
-    "Low": 70,
-    "Avg": 79
+    "Historical Avg": 79,
+    "Actual Avg": 83
   },
   {
     "Month": "Aug2023",
-    "High": 89,
-    "Low": 69,
-    "Avg": 88
+    "Historical Avg": 88,
+    "Actual Avg": 86
   },
   {
     "Month": "Sep2023",
-    "High": 82,
-    "Low": 60,
-    "Avg": 72
+    "Historical Avg": 72,
+    "Actual Avg": 75
   }
  ];
 
@@ -94,15 +81,15 @@ let weather = [
       "outage_experienced": false,
       "meter_reading_start": 117850,
       "meter_reading_end": 121623,
-      "usage_units": 3773,
-      "usage_cost": 415.03,
+      "kwh_used": 1326,
+      "usage_cost": 145.86,
       "Fees": 18,
-      "total_amount_due": 433.03,
+      "total_amount_due": 163.86,
       "service_period_ending": "9/30/2022",
       "payment_due_date": "10/31/2022",
       "payment_date": "11/11/2022",
-      "late_fee": 8.6606,
-      "payment_amount": 441.6906,
+      "late_fee": 3.2772,
+      "payment_amount": 167.1372,
       "payment_method": "credit card"
     },
     {
@@ -113,15 +100,15 @@ let weather = [
       "outage_experienced": false,
       "meter_reading_start": 121623,
       "meter_reading_end": 125070,
-      "usage_units": 3447,
-      "usage_cost": 379.17,
+      "kwh_used": 918,
+      "usage_cost": 100.98,
       "Fees": 18,
-      "total_amount_due": 397.17,
+      "total_amount_due": 118.98,
       "service_period_ending": "10/31/2022",
       "payment_due_date": "11/30/2022",
       "payment_date": "11/28/2022",
       "late_fee": 0,
-      "payment_amount": 397.17,
+      "payment_amount": 118.98,
       "payment_method": "credit card"
     },
     {
@@ -132,15 +119,15 @@ let weather = [
       "outage_experienced": true,
       "meter_reading_start": 125070,
       "meter_reading_end": 128479,
-      "usage_units": 3409,
-      "usage_cost": 374.99,
+      "kwh_used": 799,
+      "usage_cost": 87.89,
       "Fees": 18,
-      "total_amount_due": 392.99,
+      "total_amount_due": 105.89,
       "service_period_ending": "11/30/2022",
       "payment_due_date": "12/31/2022",
       "payment_date": "12/15/2022",
       "late_fee": 0,
-      "payment_amount": 392.99,
+      "payment_amount": 105.89,
       "payment_method": "bank transfer"
     },
     {
@@ -151,15 +138,15 @@ let weather = [
       "outage_experienced": false,
       "meter_reading_start": 128479,
       "meter_reading_end": 131258,
-      "usage_units": 2779,
-      "usage_cost": 305.69,
+      "kwh_used": 629,
+      "usage_cost": 69.19,
       "Fees": 18,
-      "total_amount_due": 323.69,
+      "total_amount_due": 87.19,
       "service_period_ending": "12/31/2022",
       "payment_due_date": "1/31/2023",
       "payment_date": "1/15/2023",
       "late_fee": 0,
-      "payment_amount": 323.69,
+      "payment_amount": 87.19,
       "payment_method": "credit card"
     },
     {
@@ -170,15 +157,15 @@ let weather = [
       "outage_experienced": true,
       "meter_reading_start": 131258,
       "meter_reading_end": 134076,
-      "usage_units": 2818,
-      "usage_cost": 309.98,
+      "kwh_used": 697,
+      "usage_cost": 76.67,
       "Fees": 18,
-      "total_amount_due": 327.98,
+      "total_amount_due": 94.67,
       "service_period_ending": "1/31/2023",
       "payment_due_date": "2/28/2023",
       "payment_date": "3/1/2023",
-      "late_fee": 6.5596,
-      "payment_amount": 334.5396,
+      "late_fee": 1.8934,
+      "payment_amount": 96.5634,
       "payment_method": "bank transfer"
     },
     {
@@ -189,15 +176,15 @@ let weather = [
       "outage_experienced": true,
       "meter_reading_start": 134076,
       "meter_reading_end": 137306,
-      "usage_units": 3230,
-      "usage_cost": 355.3,
+      "kwh_used": 850,
+      "usage_cost": 93.5,
       "Fees": 18,
-      "total_amount_due": 373.3,
+      "total_amount_due": 111.5,
       "service_period_ending": "2/28/2023",
       "payment_due_date": "3/31/2023",
       "payment_date": "4/5/2023",
-      "late_fee": 7.466,
-      "payment_amount": 380.766,
+      "late_fee": 2.23,
+      "payment_amount": 113.73,
       "payment_method": "credit card"
     },
     {
@@ -208,15 +195,15 @@ let weather = [
       "outage_experienced": false,
       "meter_reading_start": 137306,
       "meter_reading_end": 140154,
-      "usage_units": 2848,
-      "usage_cost": 313.28,
+      "kwh_used": 765,
+      "usage_cost": 84.15,
       "Fees": 18,
-      "total_amount_due": 331.28,
+      "total_amount_due": 102.15,
       "service_period_ending": "3/31/2023",
       "payment_due_date": "4/30/2023",
       "payment_date": "4/13/2023",
       "late_fee": 0,
-      "payment_amount": 331.28,
+      "payment_amount": 102.15,
       "payment_method": "bank transfer"
     },
     {
@@ -227,15 +214,15 @@ let weather = [
       "outage_experienced": false,
       "meter_reading_start": 140154,
       "meter_reading_end": 142104,
-      "usage_units": 1950,
-      "usage_cost": 214.5,
+      "kwh_used": 1105,
+      "usage_cost": 121.55,
       "Fees": 18,
-      "total_amount_due": 232.5,
+      "total_amount_due": 139.55,
       "service_period_ending": "4/30/2023",
       "payment_due_date": "5/31/2023",
       "payment_date": "6/11/2023",
-      "late_fee": 4.65,
-      "payment_amount": 237.15,
+      "late_fee": 2.791,
+      "payment_amount": 142.341,
       "payment_method": "bank transfer"
     },
     {
@@ -246,15 +233,15 @@ let weather = [
       "outage_experienced": true,
       "meter_reading_start": 142104,
       "meter_reading_end": 144439,
-      "usage_units": 2335,
-      "usage_cost": 256.85,
+      "kwh_used": 1190,
+      "usage_cost": 130.9,
       "Fees": 18,
-      "total_amount_due": 274.85,
+      "total_amount_due": 148.9,
       "service_period_ending": "5/31/2023",
       "payment_due_date": "6/30/2023",
       "payment_date": "6/21/2023",
       "late_fee": 0,
-      "payment_amount": 274.85,
+      "payment_amount": 148.9,
       "payment_method": "bank transfer"
     },
     {
@@ -265,15 +252,15 @@ let weather = [
       "outage_experienced": true,
       "meter_reading_start": 144439,
       "meter_reading_end": 147970,
-      "usage_units": 3531,
-      "usage_cost": 388.41,
+      "kwh_used": 1224,
+      "usage_cost": 134.64,
       "Fees": 18,
-      "total_amount_due": 406.41,
+      "total_amount_due": 152.64,
       "service_period_ending": "6/30/2023",
       "payment_due_date": "7/31/2023",
       "payment_date": "7/16/2023",
       "late_fee": 0,
-      "payment_amount": 406.41,
+      "payment_amount": 152.64,
       "payment_method": "credit card"
     },
     {
@@ -284,15 +271,15 @@ let weather = [
       "outage_experienced": false,
       "meter_reading_start": 147970,
       "meter_reading_end": 150865,
-      "usage_units": 2895,
-      "usage_cost": 318.45,
+      "kwh_used": 1411,
+      "usage_cost": 155.21,
       "Fees": 18,
-      "total_amount_due": 336.45,
+      "total_amount_due": 173.21,
       "service_period_ending": "7/31/2023",
       "payment_due_date": "8/31/2023",
       "payment_date": "9/4/2023",
-      "late_fee": 6.729,
-      "payment_amount": 343.179,
+      "late_fee": 3.4642,
+      "payment_amount": 176.6742,
       "payment_method": "bank transfer"
     },
     {
@@ -303,15 +290,15 @@ let weather = [
       "outage_experienced": false,
       "meter_reading_start": 150865,
       "meter_reading_end": 152770,
-      "usage_units": 1905,
-      "usage_cost": 209.55,
+      "kwh_used": 1462,
+      "usage_cost": 160.82,
       "Fees": 18,
-      "total_amount_due": 227.55,
+      "total_amount_due": 178.82,
       "service_period_ending": "8/31/2023",
       "payment_due_date": "9/30/2023",
       "payment_date": "9/23/2023",
       "late_fee": 0,
-      "payment_amount": 227.55,
+      "payment_amount": 178.82,
       "payment_method": "credit card"
     },
     {
@@ -322,10 +309,10 @@ let weather = [
       "outage_experienced": false,
       "meter_reading_start": 152770,
       "meter_reading_end": 156165,
-      "usage_units": 3395,
-      "usage_cost": 373.45,
+      "kwh_used": 1275,
+      "usage_cost": 140.25,
       "Fees": 18,
-      "total_amount_due": 391.45,
+      "total_amount_due": 158.25,
       "service_period_ending": "9/30/2023",
       "payment_due_date": "10/31/2023",
       "payment_date": "",
@@ -333,6 +320,6 @@ let weather = [
       "payment_amount": null,
       "payment_method": ""
     }
-   ];
+   ]
 
    module.exports = { getInvoices, getWeather };
