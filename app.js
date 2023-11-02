cc4f043a884ab8f10f1986c153994a304051703c
Trey Flowers <112641221+wflowersSFDC@users.noreply.github.com>
2023-11-02 10:10:45 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+{
+  "name": "Very Simple Node.js API Server",
+  "description": "A VERY simple node.js api server for demoing external connectivity from Salesforce",
+  "repository": "https://github.com/wflowersSFDC/Heroku_Node_TestApi",
+  "keywords": ["node", "express"]
+}
