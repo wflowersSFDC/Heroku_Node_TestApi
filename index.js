67609cf625c661c3b85c5d39cf076108e6351234
Trey Flowers <wflowers@wflower-ltmqpw7.internal.salesforce.com>
2024-02-08 14:42:52 -0500

    meeting minutes base

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const data = require('./mockdata.js');
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const data = require("./mockdata.js");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.get('/invoices', (req, res) => {
+app.get("/invoices", (req, res) => {
   return res.send(data.getInvoices());
-}); 
+});
 
-app.get('/weather', (req, res) => {
+app.get("/weather", (req, res) => {
   return res.send(data.getWeather());
-}); 
+});
+
+app.get("/meetingMinutes", (req, res) => {
+  return res.send("meeting notes!");
+});
 
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
